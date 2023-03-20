fb3b53cc14e125f8f147c8db3940469fc8054a40
alek531 <75718000+alek531@users.noreply.github.com>
2023-03-20 14:30:00 -0300

    Delete SyncFunctionGraficoCustomizado.dart

--- lib/public/SyncFunctionGraficoCustomizado.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-
-class SyncFuctionGraficoCustomizado extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SfCartesianChart();
-  }
-}
